d82931c6a20b29b16a432063a242c865bb8408a6
march-dave <ysangy@gmail.com>
2017-01-14 18:19:42 -0500

    Layer adjust

--- index.ios.js
+++ index.ios.js
@@ -8,19 +8,39 @@ import {
 } from 'react-native';
 
 export default class polnexus extends Component {
+
+  constructor(props) {
+    super(props)
+  }
+
+  onImageClick(e) {
+    console.log('e');
+  }
+
   render() {
     return (
-      <View style={styles.container}>
+      // <View style={styles.container}>
+      //   <Image onClick={this.onImageClick.bind(this)}
+      //     style={styles.goImages}
+      //     resizeMode={"contain"}
+      //     source={{uri:'https://res.cloudinary.com/drrwovgv6/image/upload/v1483895904/icon_r5rwee.png'}}
+      //   />
+      // </View>
 
-        <Image
-          style={{
-            width: 300,
-            height: 200,
-          }}
-          resizeMode={"contain"}
-          source={{uri:'https://res.cloudinary.com/drrwovgv6/image/upload/v1483895904/icon_r5rwee.png'}}
-        />
-
+      <View style={{
+        flex: 1,
+        flexDirection: 'column',
+        justifyContent: 'space-between',
+      }}>
+        <View>
+          <Image
+              style={styles.goImages}
+              resizeMode={"contain"}
+              source={{uri:'https://res.cloudinary.com/drrwovgv6/image/upload/v1483895904/icon_r5rwee.png'}}
+            />
+        </View>
+        <View style={{width: 50, height: 50, backgroundColor: 'skyblue'}} />
+        <View style={{width: 50, height: 50, backgroundColor: 'steelblue'}} />
       </View>
     );
   }
@@ -29,20 +49,14 @@ export default class polnexus extends Component {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    justifyContent: 'center',
     alignItems: 'center',
     backgroundColor: '#F5FCFF',
   },
-  welcome: {
-    fontSize: 20,
-    textAlign: 'center',
-    margin: 10,
-  },
-  instructions: {
-    textAlign: 'center',
-    color: '#333333',
-    marginBottom: 5,
-  },
+  goImages: {
+    width: 300,
+    height: 200,
+  }
+
 });
 
 AppRegistry.registerComponent('polnexus', () => polnexus);
