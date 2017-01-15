917592e368c8402bcc7836d9ae5d55a1051af2d3
march-dave <ysangy@gmail.com>
2017-01-15 10:29:46 -0500

    add api call to clickable

--- index.ios.js
+++ index.ios.js
@@ -20,14 +20,53 @@ export default class polnexus extends Component {
   }
 
   ImageClick(e) {
-    console.log('e', e);
+    // AlertIOS.alert('onPressssssss', '111');
 
-    AlertIOS.alert('onPressssssss', '111');
+    // var settings = {
+    //   "async": true,
+    //   "crossDomain": true,
+    //   "url": "http://lpolnexusserver.herokuapp.com/api/clients/587b64ba2d0aaaedc92f6614",
+    //   "method": "PUT",
+    //   "headers": {
+    //     "content-type": "application/x-www-form-urlencoded",
+    //     "cache-control": "no-cache",
+    //     "postman-token": "b6c398da-2a79-298a-fa46-3dc7fa64d021"
+    //   },
+    //   "data": {
+    //     "like": "gg"
+    //   }
+    // }
+    //
+    // $.ajax(settings).done(function (response) {
+    //   console.log(response);
+    // });
+
+    fetch('https://polnexusserver.herokuapp.com/api/clients/587b66786c867e001150cd10', {
+      method: 'PUT',
+      headers: {
+        'Accept': 'application/json',
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify({
+        like: 10,
+        dislike: 2,
+      })
+    })
+
+    // return fetch('https://polnexusserver.herokuapp.com/api/clients')
+    //       .then((response) => response.json())
+    //       .then((responseJson) => {
+    //         console.log(responseJson);
+    //         return responseJson.movies;
+    //       })
+    //       .catch((error) => {
+    //         console.error(error);
+    //       });
   }
 
   render() {
     return (
-    
+
       <View style={{
         flex: 1,
         flexDirection: 'column',
@@ -43,13 +82,12 @@ export default class polnexus extends Component {
             <Image
                 style={styles.goImages}
                 resizeMode={"contain"}
-                source={{uri:'https://res.cloudinary.com/drrwovgv6/image/upload/v1483895904/icon_r5rwee.png'}}
+                source={{uri:'https://res.cloudinary.com/drrwovgv6/image/upload/v1484491467/bluestar_pj5ecu.png'}}
               />
           </TouchableHighlight>
         </View>
 
         <View style={{width: 50, height: 50, backgroundColor: 'skyblue'}} />
-
         <Text style={ {height: 50, backgroundColor: 'steelblue'} }>{this.state.sentence}</Text>
 
       </View>
